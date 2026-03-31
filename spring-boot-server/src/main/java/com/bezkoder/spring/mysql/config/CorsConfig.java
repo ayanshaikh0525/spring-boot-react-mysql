9fdcb5cac614346c3c0ce78cb83ca83d6efc86aa
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-03-31 18:51:50 +0530

    Create CorsConfig.java

---
+++ spring-boot-server/src/main/java/com/bezkoder/spring/mysql/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.bezkoder.spring.mysql.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://spring-boot-react-mysql.vercel.app",
+                            "https://d2rfzugb63345i.cloudfront.net"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
